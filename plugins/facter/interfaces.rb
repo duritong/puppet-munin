ded0ef9de27bda5ea6c722991054b47ed3b8fc13
mh <mh@d66ca3ae-40d7-4aa7-90d4-87d79ca94279>
2008-01-31 18:05:28 +0000

    reverted namechange
    git-svn-id: https://svn/ipuppet/trunk/modules/munin@642 d66ca3ae-40d7-4aa7-90d4-87d79ca94279

--- plugins/facter/interfaces.rb
+++ plugins/facter/interfaces.rb
@@ -1,5 +1,5 @@
 # return the set of active interfaces as an array
-Facter.add("facterinterfaces") do
+Facter.add("interfaces") do
 	setcode do
 		`ip -o link show`.split(/\n/).collect do |line|
 			value = nil
