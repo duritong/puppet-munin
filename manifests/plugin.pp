bd54bb182afef2fd13e0159c67dcbc7375bfd577
mh <mh@immerda.ch>
2008-02-06 20:03:47 +0000

    removed unneeded definition

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -102,30 +102,6 @@ define munin::remoteplugin($ensure = "present", $source, $config = '') {
 }
 
 class munin::plugins::base {
-
-    case $operatingsystem {
-        gentoo: {
-            $munin_node_package = "munin"
-            $munin_node_service = "munin"
-            $script_path =  "/usr/libexec/munin/plugins"
-            }
-        debian: {
-            $munin_node_service = "munin-node"
-            $munin_node_package = "munin-node"
-            $script_path =  "/usr/share/munin/plugins"
-            }
-        centos: {
-            $munin_node_service = "munin-node"
-            $munin_node_package = "munin-node"
-            $script_path =  "/usr/share/munin/plugins"
-            }
-        default: {
-            $munin_node_service = "munin-node"
-            $munin_node_package = "munin-node"
-            $script_path =  "/usr/share/munin/plugins"
-        }
-    }
-
 	case $operatingsystem {
 		centos: {		
 		    file {
