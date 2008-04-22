c6f6cca89f7a89b5ba72b0bd65e469c93e3cf3ce
Marcel Haerry <haerry@puzzle.ch>
2008-04-22 15:01:48 +0200

    added nagios class

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -229,3 +229,11 @@ class munin::plugins::postgres inherits munin::plugins::base {
     munin::plugin::deploy { "pg__connections": ensure => false }
     munin::plugin::deploy { "pg__locks": ensure => false }
 }
+class munin::plugins::nagios inherits munin::plugins::base {
+    munin::plugin {
+        nagios_hosts: script_path => "/usr/local/bin", config => 'user root';
+        nagios_svc: script_path => "/usr/local/bin", config => 'user root';
+        nagios_perf_hosts: ensure => nagios_perf_, script_path => "/usr/local/bin", config => 'user root';
+        nagios_perf_svc: ensure => nagios_perf_, script_path => "/usr/local/bin", config => 'user root';
+    }
+}
