ebdcae0d53a7ee50e2abb2f6606f731cb76c83e8
Marcel Haerry <haerry@puzzle.ch>
2008-04-22 11:28:54 +0200

    empty module $MODULENAME

---
+++ manifests/init.pp
@@ -0,0 +1,9 @@
+#######################################
+# munin module
+# Puzzle ITC - haerry+puppet(at)puzzle.ch
+# GPLv3
+#######################################
+
+
+# modules_dir { "munin": }
+class munin {}
