70e9b0703c41ebbd7f9902488f41c3d90e77981a
Marcel Haerry <haerry@puzzle.ch>
2008-04-22 15:41:02 +0200

    added new xen plugin

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -195,6 +195,7 @@ class munin::plugins::dom0 inherits munin::plugins::physical {
     munin::plugin::deploy { "xen-cpu": config => "user root"}
     munin::plugin::deploy { "xen_memory": config => "user root"}
     munin::plugin::deploy { "xen_vbd": config => "user root"}
+    munin::plugin::deploy { "xen_traffic_all": config => "user root"}
 }
 
 class munin::plugins::physical inherits munin::plugins::base {
