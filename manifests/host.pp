3c6cb0e28220543cfd91a3cc7c02623f05a0ebb8
Marcel Haerry <haerry@puzzle.ch>
2008-07-21 16:54:58 +0200

    group writing should be set recursive

--- manifests/host.pp
+++ manifests/host.pp
@@ -38,7 +38,7 @@ class munin::host
 
 class munin::host::cgi {
     exec{'set_modes_for_cgi':
-        command => 'chgrp apache /var/log/munin /var/log/munin/munin-graph.log && chmod g+w /var/log/munin /var/log/munin/munin-graph.log && find /var/www/html/munin/* -maxdepth 1 -type d -exec chgrp -R apache {} \; && find /var/www/html/munin/* -maxdepth 1 -type d -exec chmod g+w {} \;',
+        command => 'chgrp apache /var/log/munin /var/log/munin/munin-graph.log && chmod g+w /var/log/munin /var/log/munin/munin-graph.log && find /var/www/html/munin/* -maxdepth 1 -type d -exec chgrp -R apache {} \; && find /var/www/html/munin/* -maxdepth 1 -type d -exec chmod -R g+w {} \;',
         refreshonly => true,
         subscribe => File['/etc/munin/munin.conf.header'],
     }
