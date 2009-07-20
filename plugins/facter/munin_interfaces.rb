3121bc68df6df329608464c6f3bf316917cbe813
David Schmitt <david@dasz.at>
2009-07-20 11:41:20 +0200

    avoid passing on the superflous @... suffixes on interfaces

--- plugins/facter/munin_interfaces.rb
+++ plugins/facter/munin_interfaces.rb
@@ -6,6 +6,9 @@ Facter.add("munin_interfaces") do
 			matches = line.match(/^\d*: ([^:]*): <(.*,)?UP(,.*)?>/)
 			if !matches.nil?
 				value = matches[1]
+				# remove superfluous "underlying interface" specification 
+				# for VLAN interfaces
+				value.gsub!(/@.*/, '')
 			end
 			value
 		end.compact.sort.join(" ")
