54af63deb16a02bfc8611e444bb3a64e12757f47
pietro <pietro@riseup.net>
2009-11-27 18:43:54 -0800

    update source path

--- manifests/plugin.pp
+++ manifests/plugin.pp
@@ -70,7 +70,7 @@ class munin::plugins::base {
 
 	file {
 		[ "/etc/munin/plugins", "/etc/munin/plugin-conf.d" ]:
-			source => "puppet://$server/modules/common/empty",
+			source => "puppet://$server/common/empty",
 			ensure => directory, checksum => mtime,
 			ignore => '.ignore',
 			recurse => true, purge => true, force => true, 
