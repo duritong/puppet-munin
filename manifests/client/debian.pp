ebfde169e0ef4fc749cd6804e707103fab7dbf78
Micah Anderson <micah@riseup.net>
2009-12-22 19:50:28 -0500

    take out the removal of the postfix_mailvolume munin plugin. I do not see what the bug is, and this interferes with local installation of the plugin (causes duplicate definitions)

--- manifests/client/debian.pp
+++ manifests/client/debian.pp
@@ -9,7 +9,5 @@ class munin::client::debian inherits munin::client::package {
     File["/etc/munin/munin-node.conf"]{
 			content => template("munin/munin-node.conf.$operatingsystem.$lsbdistcodename"),
     }
-	# workaround bug in munin_node_configure
-	plugin { "postfix_mailvolume": ensure => absent }
 	include munin::plugins::debian
 }
