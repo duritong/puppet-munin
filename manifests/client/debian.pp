511afffde2c5375fd685133f6c697c3ffeaaed8b
Micah Anderson <micah@riseup.net>
2009-12-22 19:59:02 -0500

    indentation fixups, using the emacs/vim modes for standardization

--- manifests/client/debian.pp
+++ manifests/client/debian.pp
@@ -1,13 +1,13 @@
 class munin::client::debian inherits munin::client::package {
-    # the plugin will need that
-	package { "iproute": ensure => installed }
-
-	Service["munin-node"]{
-		# sarge's munin-node init script has no status
-		hasstatus => $lsbdistcodename ? { sarge => false, default => true }
-	}
-    File["/etc/munin/munin-node.conf"]{
-			content => template("munin/munin-node.conf.$operatingsystem.$lsbdistcodename"),
-    }
-	include munin::plugins::debian
+  # the plugin will need that
+  package { "iproute": ensure => installed }
+  
+  Service["munin-node"]{
+    # sarge's munin-node init script has no status
+    hasstatus => $lsbdistcodename ? { sarge => false, default => true }
+  }
+  File["/etc/munin/munin-node.conf"]{
+    content => template("munin/munin-node.conf.$operatingsystem.$lsbdistcodename"),
+  }
+  include munin::plugins::debian
 }
