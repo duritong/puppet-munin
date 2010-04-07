c1e5fe085837af4c5569404619c77310d4285253
Jerome Charaoui <jcharaoui@cmaisonneuve.qc.ca>
2010-04-07 10:58:12 -0400

    add ability to register a non-puppet-managed munin node

--- manifests/register.pp
+++ manifests/register.pp
@@ -1,10 +1,23 @@
-define munin::register()
+define munin::register(
+  $host = 'absent',
+  $port = 'absent'
+)
 {
-	$munin_port_real = $munin_port ? { '' => 4949, default => $munin_port } 
-	$munin_host_real = $munin_host ? {
-		'' => $fqdn,
-		'fqdn' => $fqdn,
-		default => $munin_host
+	$munin_port_real = $port ? {
+    'absent' => $munin_port ? {
+                  '' => 4949,
+                  default => $munin_port
+                },
+    default => $port
+  }
+
+	$munin_host_real = $host ? {
+    'absent' => $munin_host ? {
+                  '' => $fqdn,
+                  'fqdn' => $fqdn,
+                  default => $munin_host
+                },
+    default => $host
 	}
 
 	@@file { "/var/lib/puppet/modules/munin/nodes/${name}_${munin_port_real}":
