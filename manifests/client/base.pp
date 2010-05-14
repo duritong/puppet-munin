f1586a7fbcc79fee6542790b1f4c930929af7e04
Jerome Charaoui <jcharaoui@cmaisonneuve.qc.ca>
2010-05-14 11:02:03 -0400

    refactor register class and client template

--- manifests/client/base.pp
+++ manifests/client/base.pp
@@ -18,6 +18,8 @@ class munin::client::base {
         notify => Service['munin-node'],
         mode => 0644, owner => root, group => 0,
     }
-    munin::register { $fqdn: }
+    munin::register { $fqdn:
+        config => [ 'use_node_name yes', 'load.load.warning 5', 'load.load.critical 10'],
+    }
     include munin::plugins::base
 }
