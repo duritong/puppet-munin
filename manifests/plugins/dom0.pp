892232f7c419fed071b7b1bdb0e3a2b9482c7449
mh <mh@immerda.ch>
2010-07-19 21:41:55 +0200

    fixing the actual problem

--- manifests/plugins/dom0.pp
+++ manifests/plugins/dom0.pp
@@ -1,7 +1,7 @@
 class munin::plugins::dom0 {
   munin::plugin::deploy {
     [ 'xen', 'xen-cpu', 'xen_memory', 'xen_mem',
-      'xen_vm', 'xen_vbd' 'xen_traffic_all' ]:
+      'xen_vm', 'xen_vbd', 'xen_traffic_all' ]:
       config => 'user root';
   }
 }
