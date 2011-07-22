eb953716cd545f86972e85d983b5862c194f5aed
mh <mh@immerda.ch>
2011-07-22 13:04:38 +0200

    fix typo

--- manifests/plugins/apache.pp
+++ manifests/plugins/apache.pp
@@ -1,4 +1,4 @@
 class munin::plugins::apache {
-  munin::plugin{ [ 'apache_accesses', 'apache_processes',' apache_volume' ]: }
-  munin::plugin::deploy { "apache_activity": }
+  munin::plugin{ [ 'apache_accesses', 'apache_processes', 'apache_volume' ]: }
+  munin::plugin::deploy { 'apache_activity': }
 }
