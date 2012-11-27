bc23eb26287c343358a6bfbb9a776a67db39d02e
mh <mh@immerda.ch>
2012-11-27 22:08:14 +0100

    improve regexp

--- manifests/plugins/interfaces.pp
+++ manifests/plugins/interfaces.pp
@@ -2,20 +2,21 @@
 class munin::plugins::interfaces  {
 
   # filter out many of the useless interfaces that show up
-  $ifs = regsubst(reject(split($::interfaces, ' |,'), 'eth\d+:\d+|sit0|virbr\d+_nic|vif\d+_\d+|veth\d+'), '(.+)', 'if_\\1')
+  $real_ifs = reject(split($::interfaces, ' |,'), 'eth\d+:\d+|sit0|virbr\d+_nic|vif\d+_\d+|veth\d+')
+  $ifs = regsubst($real_ifs, '(.+)', "if_\\1")
 
   munin::plugin {
     $ifs: ensure => 'if_';
   }
   case $::operatingsystem {
     openbsd: {
-      $if_errs = regsubst(split($::interfaces, ' |,'), '(.+)', 'if_errcoll_\\1')
+      $if_errs = regsubst($real_ifs, '(.+)', "if_errcoll_\\1")
       munin::plugin{
         $if_errs: ensure => 'if_errcoll_';
       }
     }
     default: {
-      $if_errs = regsubst(split($::interfaces, ' |,'), '(.+)', 'if_err_\\1')
+      $if_errs = regsubst($real_ifs, '(.+)', "if_err_\\1")
       munin::plugin{
         $if_errs: ensure => 'if_err_';
       }
