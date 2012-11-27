adbfc508abf5bda607795f7bf02f9581e78b17a5
mh <mh@immerda.ch>
2012-11-27 21:54:19 +0100

    filter out many of the useless interfaces that show up

--- manifests/plugins/interfaces.pp
+++ manifests/plugins/interfaces.pp
@@ -1,21 +1,23 @@
 # handle if_ and if_err_ plugins
-class munin::plugins::interfaces  { 
+class munin::plugins::interfaces  {
 
-  $ifs = regsubst(split($::interfaces, " |,"), "(.+)", "if_\\1")
+  # filter out many of the useless interfaces that show up
+  $ifs = regsubst(reject(split($::interfaces, ' |,'), 'eth\d+:\d+|sit0|virbr\d+_nic|vif\d+_\d+|veth\d+'), '(.+)', 'if_\\1')
+
   munin::plugin {
-    $ifs: ensure => "if_";
+    $ifs: ensure => 'if_';
   }
   case $::operatingsystem {
     openbsd: {
-      $if_errs = regsubst(split($::interfaces, " |,"), "(.+)", "if_errcoll_\\1")
+      $if_errs = regsubst(split($::interfaces, ' |,'), '(.+)', 'if_errcoll_\\1')
       munin::plugin{
-        $if_errs: ensure => "if_errcoll_";
+        $if_errs: ensure => 'if_errcoll_';
       }
     }
     default: {
-      $if_errs = regsubst(split($::interfaces, " |,"), "(.+)", "if_err_\\1")
+      $if_errs = regsubst(split($::interfaces, ' |,'), '(.+)', 'if_err_\\1')
       munin::plugin{
-        $if_errs: ensure => "if_err_";
+        $if_errs: ensure => 'if_err_';
       }
     }
   }
