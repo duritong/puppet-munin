630f4d609e1e58a74abb04963ce68c9fc4bab4a1
mh <mh@immerda.ch>
2012-12-06 22:11:10 +0100

    replace xen-cpu plugin with a better plugin

--- manifests/plugins/dom0.pp
+++ manifests/plugins/dom0.pp
@@ -1,6 +1,6 @@
 class munin::plugins::dom0 {
   munin::plugin::deploy {
-    [ 'xen', 'xen-cpu', 'xen_memory', 'xen_mem',
+    [ 'xen', 'xen_cpu', 'xen_memory', 'xen_mem',
       'xen_vm', 'xen_vbd', 'xen_traffic_all' ]:
       config => 'user root';
   }
