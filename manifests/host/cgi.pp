8a956cc2f1ad97d06e9a267312e55260ecb29eac
Sander Hoentjen <sander@hoentjen.eu>
2013-03-25 14:46:25 +0100

    linting

--- manifests/host/cgi.pp
+++ manifests/host/cgi.pp
@@ -1,6 +1,7 @@
 class munin::host::cgi {
   exec{'set_modes_for_cgi':
     command => 'chgrp apache /var/log/munin /var/log/munin/munin-graph.log && chmod g+w /var/log/munin /var/log/munin/munin-graph.log && find /var/www/html/munin/* -maxdepth 1 -type d -exec chgrp -R apache {} \; && find /var/www/html/munin/* -maxdepth 1 -type d -exec chmod -R g+w {} \;',
+    command     => 'chgrp apache /var/log/munin /var/log/munin/munin-graph.log && chmod g+w /var/log/munin /var/log/munin/munin-graph.log && find /var/www/html/munin/* -maxdepth 1 -type d -exec chgrp -R apache {} \; && find /var/www/html/munin/* -maxdepth 1 -type d -exec chmod -R g+w {} \;',
     refreshonly => true,
     subscribe   => Concat::Fragment['munin.conf.header'],
   }
