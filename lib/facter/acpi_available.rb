41be4c9b05b95fc0326ec19ebae0fdffea151f7c
Eric Badger <badgerea@hotmail.com>
2013-08-19 18:20:46 -0500

    Use Facter::Util::Resolution.which instead of `which ...`
    If this plugin is synced to systems without the 'which' executable,
    backticked which will cause error messages.

--- lib/facter/acpi_available.rb
+++ lib/facter/acpi_available.rb
@@ -1,7 +1,7 @@
 # return whether acpi is available -- used for deciding whether to install the munin plugin
 Facter.add("acpi_available") do
     setcode do
-    	if not File.exist? `which acpi 2>/dev/null`.chomp or `acpi -t -B -A 2>/dev/null`.match(/\d/).nil?
+    	if not Facter::Util::Resolution.which('acpi') or `acpi -t -B -A 2>/dev/null`.match(/\d/).nil?
     		"absent"
     	else
     		"present"
