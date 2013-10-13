342ea7aa15a54c33ec4a12cf10b7b1a0c9811dba
Tomas Barton <barton.tomas@gmail.com>
2013-10-13 18:14:45 +0200

    extended tests, first group should pass

--- spec/classes/munin_client_spec.rb
+++ spec/classes/munin_client_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe 'munin::client' do
-  shared_examples 'debian' do |os, codename|
+  shared_examples 'debian-client' do |os, codename|
     let(:facts) {{
       :operatingsystem => os,
       :osfamily => 'Debian',
@@ -10,9 +10,10 @@ describe 'munin::client' do
     it { should contain_package('munin-node') }
     it { should contain_package('iproute') }
     it { should contain_file('/etc/munin/munin-node.conf') }
+    it { should include_class('munin::client::debian') }
   end
 
-  shared_examples 'redhat' do |os, codename|
+  shared_examples 'redhat-client' do |os, codename|
     let(:facts) {{
       :operatingsystem => os,
       :osfamily        => 'RedHat',
@@ -24,13 +25,13 @@ describe 'munin::client' do
   end
 
   context 'on debian-like system' do
-    it_behaves_like 'debian', 'Debian', 'squeeze'
-    it_behaves_like 'debian', 'Debian', 'wheezy'
-    it_behaves_like 'debian', 'Ubuntu', 'precise'
+    it_behaves_like 'debian-client', 'Debian', 'squeeze'
+    it_behaves_like 'debian-client', 'Debian', 'wheezy'
+    it_behaves_like 'debian-client', 'Ubuntu', 'precise'
   end
 
   context 'on redhat-like system' do
-    it_behaves_like 'redhat', 'CentOS', '6'
+    it_behaves_like 'redhat-client', 'CentOS', '6'
     # not supported yet
     # it_behaves_like 'redhat', 'RedHat', '6'
   end
@@ -44,6 +45,7 @@ describe 'munin::client' do
     }}
     it { should contain_package('munin-node') }
     it { should contain_file('/etc/munin/munin-node.conf') }
+    it { should include_class('munin::client::gentoo') }
   end
 
 end
--- spec/classes/munin_host_spec.rb
+++ spec/classes/munin_host_spec.rb
@@ -1,14 +1,21 @@
 require 'spec_helper'
 
 describe 'munin::host' do
-  let :facts do
-    {
-      :operatingsystem => 'CentOS',
-      :interfaces      => 'lo,eth0',
-    }
+  shared_examples 'debian-host' do |os, codename|
+    let(:facts) {{
+      :operatingsystem => os,
+      :osfamily => 'Debian',
+      :lsbdistcodename => codename,
+      :concat_basedir => '/var/lib/puppet/concat',
+    }}
+    it { should contain_package('munin') }
+    it { should contain_file('/etc/munin/munin.conf') }
   end
 
-  it 'should compile' do
-    should include_class('munin::host')
-  end
+#  context 'on debian-like system' do
+#    it_behaves_like 'debian-host', 'Debian', 'squeeze'
+#    it_behaves_like 'debian', 'Debian', 'wheezy'
+#    it_behaves_like 'debian', 'Ubuntu', 'precise'
+#  end
+
 end
