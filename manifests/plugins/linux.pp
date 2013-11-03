a161f0cd19a4e099bc8bb4bb3e01898c9c5f2ee7
mh <mh@immerda.ch>
2013-11-03 18:43:23 +0100

    add a few more intelligent plugins for linux hosts

--- manifests/plugins/linux.pp
+++ manifests/plugins/linux.pp
@@ -1,7 +1,8 @@
 # Set up plugins for a linux host
 class munin::plugins::linux {
   munin::plugin {
-    [ 'df_abs', 'forks', 'df_inode', 'irqstats', 'entropy', 'open_inodes' ]:
+    [ 'df_abs', 'forks', 'df_inode', 'irqstats', 'entropy', 'open_inodes',
+      'diskstats', 'proc_pri', 'threads', ]:
       ensure => present;
     'acpi':
       ensure => $::acpi_available;
