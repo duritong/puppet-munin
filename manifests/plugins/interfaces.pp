03c50709169ee38cd5d759c461389b2a476fcc64
mh <mh@immerda.ch>
2013-11-03 17:52:36 +0100

    use rather the stdlib prefix function than the good old regsubst hack

--- manifests/plugins/interfaces.pp
+++ manifests/plugins/interfaces.pp
@@ -1,15 +1,16 @@
 # handle if_ and if_err_ plugins
 class munin::plugins::interfaces {
 
+  # filter out many of the useless interfaces that show up
+  $real_ifs = reject(split($::interfaces, ' |,'), 'eth\d+_\d+|sit0|virbr\d+_nic|vif\d+_\d+|veth\d+|__tmp\d+')
+
+  $ifs = prefix($real_ifs, 'if_')
+
   $if_err_plugin = $::operatingsystem ? {
     'openbsd' => 'if_errcoll_',
     default   => 'if_err_',
   }
-
-  # filter out many of the useless interfaces that show up
-  $real_ifs = reject(split($::interfaces, ' |,'), 'eth\d+_\d+|sit0|virbr\d+_nic|vif\d+_\d+|veth\d+|__tmp\d+')
-  $ifs = regsubst($real_ifs, '(.+)', 'if_\1')
-  $if_errs = regsubst($real_ifs, '(.+)', "${if_err_plugin}\1")
+  $if_errs = prefix($real_ifs, $if_err_plugin)
 
   munin::plugin { $ifs:
     ensure => 'if_',
