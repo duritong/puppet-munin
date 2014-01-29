99e32a12d401828e71c13309bf21460086f77e0e
mh <mh@immerda.ch>
2014-01-29 23:32:52 +0100

    unify logdir

--- manifests/client/openbsd.pp
+++ manifests/client/openbsd.pp
@@ -1,6 +1,6 @@
 # generate a few missing things on openbsd
 class munin::client::openbsd inherits munin::client::base {
-  file{[ '/var/run/munin', '/var/log/munin' ]:
+  file{[ '/var/run/munin', '/var/log/munin-node' ]:
     ensure  => directory,
     owner   => root,
     group   => 0,
@@ -17,10 +17,10 @@ class munin::client::openbsd inherits munin::client::base {
     status      => 'test -e /var/run/munin/munin-node.pid && (ps ax | egrep -q "^ *$(cat /var/run/munin/munin-node.pid).*munin-node")',
     hasstatus   => true,
     hasrestart  => true,
-    require     => [ File['/var/run/munin'], File['/var/log/munin'] ],
+    require     => [ File['/var/run/munin'], File['/var/log/munin-munin'] ],
   }
   cron{'clean_munin_logfile':
-    command => 'rm /var/log/munin/munin-node.log; kill -HUP `cat /var/run/munin/munin-node.pid`',
+    command => 'rm /var/log/munin-node/munin-node.log; kill -HUP `cat /var/run/munin/munin-node.pid`',
     minute  => 0,
     hour    => 2,
     weekday => 0,
