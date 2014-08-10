d9694b12175b7c4596ba3ada840f864be0db621a
Jasper Lievisse Adriaanse <jasper@humppa.nl>
2014-08-10 21:35:21 +0200

    Adjust service handling for OpenBSD
    - OpenBSD has support for rc scripts for several years now, and Puppet supports
    it for some time too now. However the service name is 'munin_node' instead of
    'munin-node' so adjust for that.

--- manifests/client/openbsd.pp
+++ manifests/client/openbsd.pp
@@ -2,23 +2,11 @@
 class munin::client::openbsd inherits munin::client::base {
   file{[ '/var/run/munin', '/var/log/munin-node' ]:
     ensure  => directory,
-    owner   => root,
-    group   => 0,
-    mode    => '0755';
+    owner   => '_munin',
+    group   => '_munin-plugin',
+    mode    => '0775';
   }
-  openbsd::rc_local{'munin-node':
-    binary  => '/usr/local/sbin/munin-node',
-    require => File['/var/run/munin'],
-  }
-  Service['munin-node']{
-    restart     => '/bin/kill -HUP `/bin/cat /var/run/munin/munin-node.pid`',
-    stop        => '/bin/kill `/bin/cat /var/run/munin/munin-node.pid`',
-    start       => '/usr/local/sbin/munin-node',
-    status      => 'test -e /var/run/munin/munin-node.pid && (ps ax | egrep -q "^ *$(cat /var/run/munin/munin-node.pid).*munin-node")',
-    hasstatus   => true,
-    hasrestart  => true,
-    require     => [ File['/var/run/munin'], File['/var/log/munin-node'] ],
-  }
+
   cron{'clean_munin_logfile':
     command => 'rm /var/log/munin-node/munin-node.log; kill -HUP `cat /var/run/munin/munin-node.pid`',
     minute  => 0,
--- manifests/client/params.pp
+++ manifests/client/params.pp
@@ -4,14 +4,17 @@ class munin::client::params {
 
   case $::osfamily {
     'OpenBSD': {
+      $service = 'munin_node'
       $group = '0'
       $log_file = '/var/log/munin-node/munin-node.log'
     }
     'Debian': {
+      $service = 'munin_node'
       $group = 'root'
       $log_file = '/var/log/munin/munin-node.log'
     }
     default: {
+      $service = 'munin_node'
       $group = 'root'
       $log_file = '/var/log/munin-node/munin-node.log'
     }
