15a1a5ad36cbcdffa0ac3aa5b99d5246892c96fc
Jasper Lievisse Adriaanse <jasper@humppa.nl>
2014-08-10 22:18:44 +0200

    Remove cron job that makes no sense anymore

--- manifests/client/openbsd.pp
+++ manifests/client/openbsd.pp
@@ -2,15 +2,8 @@
 class munin::client::openbsd inherits munin::client::base {
   file{[ '/var/run/munin', '/var/log/munin-node' ]:
     ensure  => directory,
-    owner   => '_munin',
+    owner   => '_munin-plugin',
     group   => '_munin-plugin',
     mode    => '0775';
-  }
-
-  cron{'clean_munin_logfile':
-    command => 'rm /var/log/munin-node/munin-node.log; kill -HUP `cat /var/run/munin/munin-node.pid`',
-    minute  => 0,
-    hour    => 2,
-    weekday => 0,
   }
 }
