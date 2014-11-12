84943edf06ef1b1fb067bf60e16c6b477a82b4c1
Jasper Lievisse Adriaanse <jasper@humppa.nl>
2014-11-12 14:50:05 +0100

    sync with the openbsd package

--- manifests/client/openbsd.pp
+++ manifests/client/openbsd.pp
@@ -1,9 +1,18 @@
 # generate a few missing things on openbsd
 class munin::client::openbsd inherits munin::client::base {
-  file{[ '/var/run/munin', '/var/log/munin-node' ]:
+  file{ '/var/run/munin':
+    ensure  => directory,
+    owner   => '_munin-plugin',
+    group   => '_munin',
+    mode    => '0775',
+    require => Package['munin-node'],
+  }
+
+  file{ '/var/log/munin-node':
     ensure  => directory,
     owner   => '_munin',
     group   => '_munin',
-    mode    => '0755';
+    mode    => '0755',
+    require => Package['munin-node'],
   }
 }
