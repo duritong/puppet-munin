dbbd00e2ef73441a680b6b12552f46c3096163d4
mh <mh@immerda.ch>
2015-01-09 16:58:08 +0100

    on el7 systems patch plugin for the correct location

--- manifests/plugins/selinux.pp
+++ manifests/plugins/selinux.pp
@@ -1,4 +1,11 @@
 # SELinux specific plugins
 class munin::plugins::selinux {
   munin::plugin{ [ 'selinux_avcstat' ]: }
+  if $::operatingsystemmajrelease > 6 {
+    # patch for https://github.com/munin-monitoring/munin/pull/326
+    exec{'sed -i \'s/^AVCSTATS=.*/AVCSTATS="\/sys\/fs\/selinux\/avc\/cache_stats"/\' /usr/share/munin/plugins/selinux_avcstat':
+      onlyif  => 'grep -q "AVCSTATS=\"/selinux" /usr/share/munin/plugins/selinux_avcstat',
+      require => Package['munin-node']
+    }
+  }
 }
