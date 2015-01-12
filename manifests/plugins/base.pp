9ace1a951a61242f991bb9ac120e586fdbbd8abf
mh <mh@immerda.ch>
2015-01-12 21:55:03 +0100

    make it possible to run df as root, because sometimes you might want to monitor filesystems not reachable by munin's default user nobody

--- manifests/plugins/base.pp
+++ manifests/plugins/base.pp
@@ -1,13 +1,20 @@
 # A basic set of plugins
-class munin::plugins::base {
+class munin::plugins::base(
+  $df_as_root = false
+) {
   # setup basic plugins
   munin::plugin {
     [ 'df', 'cpu', 'interrupts', 'load', 'memory', 'netstat', 'open_files',
       'processes', 'swap', 'uptime', 'users', 'vmstat' ]:
             ensure => present,
   }
+  if $df_as_root {
+    $df_opt = "user root\n"
+  } else {
+    $df_opt = ''
+  }
   file{'/etc/munin/plugin-conf.d/df':
-    content => "[df*]\nenv.exclude none unknown iso9660 squashfs udf romfs ramfs debugfs binfmt_misc rpc_pipefs fuse.gvfs-fuse-daemon\n",
+    content => "[df*]\n${df_opt}env.exclude none unknown iso9660 squashfs udf romfs ramfs debugfs binfmt_misc rpc_pipefs fuse.gvfs-fuse-daemon\n",
     require => Munin::Plugin['df'],
     owner   => 'root',
     group   => 0,
