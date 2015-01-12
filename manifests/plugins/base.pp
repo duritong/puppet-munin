1f98cbe9f122acd1066631932b62e16c87ba9364
mh <mh@immerda.ch>
2015-01-12 21:57:04 +0100

    linting

--- manifests/plugins/base.pp
+++ manifests/plugins/base.pp
@@ -14,7 +14,8 @@ class munin::plugins::base(
     $df_opt = ''
   }
   file{'/etc/munin/plugin-conf.d/df':
-    content => "[df*]\n${df_opt}env.exclude none unknown iso9660 squashfs udf romfs ramfs debugfs binfmt_misc rpc_pipefs fuse.gvfs-fuse-daemon\n",
+    content => "[df*]\n${df_opt}env.exclude none unknown iso9660 squashfs udf \
+romfs ramfs debugfs binfmt_misc rpc_pipefs fuse.gvfs-fuse-daemon\n",
     require => Munin::Plugin['df'],
     owner   => 'root',
     group   => 0,
@@ -23,18 +24,18 @@ class munin::plugins::base(
   include munin::plugins::interfaces
 
   case $::kernel {
-    openbsd: { include munin::plugins::openbsd }
-    linux: {
+    'openbsd': { include munin::plugins::openbsd }
+    'linux': {
       case $::vserver {
-        guest: { include munin::plugins::vserver }
+        'guest': { include munin::plugins::vserver }
         default: { include munin::plugins::linux }
       }
     }
   }
 
   case $::virtual {
-    physical: { include munin::plugins::physical }
-    xen0: { include munin::plugins::dom0 }
+    'physical': { include munin::plugins::physical }
+    'xen0': { include munin::plugins::dom0 }
     default: { }
   }
 }
