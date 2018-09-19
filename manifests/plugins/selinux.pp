e3a8e7bb589bd92eb9ab912e86fafdaa5c080d82
mh <mh@immerda.ch>
2018-09-19 18:44:59 +0200

    this is now fixed

--- manifests/plugins/selinux.pp
+++ manifests/plugins/selinux.pp
@@ -1,11 +1,4 @@
 # SELinux specific plugins
 class munin::plugins::selinux {
   munin::plugin{ [ 'selinux_avcstat' ]: }
-  if versioncmp($::operatingsystemmajrelease,'6') > 0 {
-    # patch for https://github.com/munin-monitoring/munin/pull/326
-    exec{'sed -i \'s/^AVCSTATS=.*/AVCSTATS="\/sys\/fs\/selinux\/avc\/cache_stats"/\' /usr/share/munin/plugins/selinux_avcstat':
-      onlyif  => 'grep -q "AVCSTATS=\"/selinux" /usr/share/munin/plugins/selinux_avcstat',
-      require => Package['munin-node']
-    }
-  }
 }
