4cd5f39716b22046e42467f3af18cc049f3c5d3d
mh <mh@immerda.ch>
2019-01-31 16:19:45 +0100

    this is not anymore needed

--- manifests/host/cgi.pp
+++ manifests/host/cgi.pp
@@ -9,16 +9,4 @@ cgiurl_graph /munin/graph\n",
       group => 0,
       mode  => '0644',
   }
-
-  exec{'set_modes_for_cgi':
-    command     => "chgrp ${apache_user} /var/log/munin /var/log/munin/munin-graph.log && chmod g+w /var/log/munin /var/log/munin/munin-graph.log && find ${document_root}/* -maxdepth 1 -type d -exec chgrp -R ${apache_user} {} \; && find ${document_root}/* -maxdepth 1 -type d -exec chmod -R g+w {} \;",
-    refreshonly => true,
-  }
-
-  file{'/etc/logrotate.d/munin':
-    content => template("munin/logrotate.conf.erb"),
-    owner   => root,
-    group   => 0,
-    mode    => '0644',
-  }
 }
