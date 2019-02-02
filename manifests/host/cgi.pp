5d8badbddaafe34bf48a61f3a1e662ffbf58bc0f
mh <mh@immerda.ch>
2019-02-02 13:34:54 +0100

    cgi needs to be able to connect to rrdcached

--- manifests/host/cgi.pp
+++ manifests/host/cgi.pp
@@ -1,12 +1,15 @@
 # Set up a munin host using CGI rendering
 class munin::host::cgi{
-  file{
+  selboolean{'nis_enabled':
+    value      => on,
+    persistent => true,
+  } -> file{
     '/etc/munin/conf.d/01_cgi.conf':
       content => "graph_strategy cgi
 html_strategy cgi
 cgiurl_graph /munin/graph\n",
-      owner => root,
-      group => 0,
-      mode  => '0644',
+      owner   => root,
+      group   => 0,
+      mode    => '0644',
   }
 }
