b59320291c527b3921d5e09751ded753ac48ee8e
mh <mh@immerda.ch>
2020-11-20 23:14:38 +0100

    modernize

--- manifests/plugins/interfaces.pp
+++ manifests/plugins/interfaces.pp
@@ -2,21 +2,15 @@
 class munin::plugins::interfaces {
 
   # filter out many of the useless interfaces that show up
-  $real_ifs = reject(split($::interfaces, ' |,'), 'eth\d+:\d+|eth\d+_\d+|sit0|virbr\d+_nic|vif\d+_\d+|veth(\w+)?\d+|__tmp\d+')
+  $real_ifs = reject(split($facts['interfaces'], ' |,'), 'eth\d+:\d+|eth\d+_\d+|sit0|virbr\d+_nic|vif\d+_\d+|veth(\w+)?\d+|__tmp\d+')
 
   $ifs = prefix($real_ifs, 'if_')
+  $if_errs = prefix($real_ifs, 'if_err')
 
-  $if_err_plugin = $::operatingsystem ? {
-    'openbsd' => 'if_errcoll_',
-    default   => 'if_err_',
-  }
-  $if_errs = prefix($real_ifs, $if_err_plugin)
-
-  munin::plugin { $ifs:
-    ensure => 'if_',
-  }
-
-  munin::plugin { $if_errs:
-    ensure => $if_err_plugin,
+  munin::plugin {
+    $ifs:
+      ensure => 'if_';
+    $if_errs:
+      ensure => 'if_err';
   }
 }
