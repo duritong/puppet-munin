fc00a0725bad4905c1018efbb8a554976d7c8b1c
mh <mh@immerda.ch>
2020-11-20 23:25:47 +0100

    use this fact as legacy interfaces fact to use a fact that returns all interfaces

--- manifests/plugins/interfaces.pp
+++ manifests/plugins/interfaces.pp
@@ -2,7 +2,7 @@
 class munin::plugins::interfaces {
 
   # filter out many of the useless interfaces that show up
-  $real_ifs = reject(split($facts['interfaces'], ' |,'), 'eth\d+:\d+|eth\d+_\d+|sit0|virbr\d+_nic|vif\d+_\d+|veth(\w+)?\d+|__tmp\d+')
+  $real_ifs = reject(split($facts['legacy_interfaces'], ','), 'eth\d+:\d+|eth\d+_\d+|sit0|virbr\d+_nic|vif\d+_\d+|veth(\w+)?\d+|__tmp\d+')
 
   $ifs = prefix($real_ifs, 'if_')
   $if_errs = prefix($real_ifs, 'if_err')
