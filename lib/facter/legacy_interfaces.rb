444cf9b1e4c225ae1a370948db773b33db96913f
mh <mh@immerda.ch>
2020-11-21 02:18:44 +0100

    proper cleanup

--- lib/facter/legacy_interfaces.rb
+++ lib/facter/legacy_interfaces.rb
@@ -2,12 +2,12 @@
 Facter.add("legacy_interfaces") do
   setcode do
     `ip -o link show`.split(/\n/).collect do |line|
-      value = nil
       matches = line.match(/^\d*: ([^:]*): <(.*,)?UP(,.*)?>/)
       if !matches.nil?
-        value = matches[1]
+        matches[1].split('@',2).first
+      else
+        nil
       end
-      value.split('@',2).first
     end.compact.sort.join(',')
   end
 end
