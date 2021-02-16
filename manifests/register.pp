f9e8b6d62b9e47b8edcfee02d90bcc4ed52dcffb
mh <mh@immerda.ch>
2021-02-16 11:58:11 +0100

    make sure to properly export ipv6 addresses

--- manifests/register.pp
+++ manifests/register.pp
@@ -1,20 +1,25 @@
 # register a client
-define munin::register(
-  $host         = $::fqdn,
+define munin::register (
+  $host         = $facts['networking']['fqdn'],
   $port         = '4949',
   $use_ssh      = false,
   $description  = 'absent',
   $config       = [],
   $group        = 'absent',
-){
+) {
   $fhost = $name
   $client_type = 'client'
-  @@file{
+  if $host =~ Stdlib::IP::Address::V6::Nosubnet {
+    $host_str = "[${host}]"
+  } else {
+    $host_str = $host
+  }
+  @@file {
     "/etc/munin/conf.d/10_${fhost}_${port}.conf":
       content => template('munin/client.erb'),
       owner   => root,
       group   => 0,
       mode    => '0644',
-      tag     => 'munin_client_register'
+      tag     => 'munin_client_register',
   }
 }
