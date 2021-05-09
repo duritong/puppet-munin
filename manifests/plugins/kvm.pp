db0e2cdbe758e6d47a6bf3285a210a7d69799516
mh <mh@immerda.ch>
2021-05-09 20:47:21 +0200

    make sure we deploy the old one for older releases

--- manifests/plugins/kvm.pp
+++ manifests/plugins/kvm.pp
@@ -1,8 +1,15 @@
 # Set up munin plugins for a KVM host
 class munin::plugins::kvm {
   munin::plugin::deploy {
-    [ 'kvm_cpu', 'kvm_mem', 'kvm_net' ]:;
+    ['kvm_cpu', 'kvm_mem', 'kvm_net']:;
     'kvm_io':
       config => 'user root';
+  }
+  if versioncmp($facts['os']['release']['major'],'8') < 0 {
+    ['kvm_cpu', 'kvm_mem', 'kvm_net', 'kvm_io'].each |$p| {
+      Munin::Plugin::Deploy[$p] {
+        source => "munin/plugins/${p}.CentOS.${facts['os']['release']['major']}",
+      }
+    }
   }
 }
