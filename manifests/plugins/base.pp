bdd94c8812f0f2ebffb2839508b67bcf1c393bc3
mh <mh@immerda.ch>
2022-08-15 22:33:25 +0200

    linting

--- manifests/plugins/base.pp
+++ manifests/plugins/base.pp
@@ -6,14 +6,14 @@ class munin::plugins::base(
   # setup basic plugins
   munin::plugin {
     [ 'df', 'cpu', 'interrupts', 'load', 'memory', 'netstat', 'open_files',
-      'processes', 'swap', 'uptime', 'users', 'vmstat' ]:
-            ensure => present,
+    'processes', 'swap', 'uptime', 'users', 'vmstat' ]:
+      ensure => present,
   }
   Package['net-tools'] -> Munin::Plugin['netstat']
   if $df_as_root {
     $df_opt = "user root\n"
   } else {
-    $df_opt = ''
+    $df_opt = undef
   }
   file{'/etc/munin/plugin-conf.d/df':
     content => "[df*]\n${df_opt}env.exclude none unknown iso9660 squashfs udf \
@@ -25,12 +25,12 @@ romfs ramfs debugfs binfmt_misc rpc_pipefs fuse.gvfs-fuse-daemon\n",
   }
   include munin::plugins::interfaces
 
-  case $::kernel {
+  case $facts['kernel'] {
     'openbsd': { include munin::plugins::openbsd }
     'linux': { include munin::plugins::linux }
   }
 
-  case $::virtual {
+  case $facts['virtual'] {
     'physical': { include munin::plugins::physical }
     'xen0': { include munin::plugins::dom0 }
     default: { }
